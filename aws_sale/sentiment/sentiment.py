9d8a0f511548a46d33c65883e944a865935b6bcf
Gilles Vieira <vieira.gille@gmail.com>
2024-03-23 17:06:26 +0100

    add base for sentiments
    Signed-off-by: Gilles Vieira <vieira.gille@gmail.com>

---
+++ aws_sale/sentiment/sentiment.py
@@ -0,0 +1,57 @@
+from typing import Callable, List
+
+from matplotlib import pyplot as plt
+from nltk import FreqDist
+from nltk.sentiment import SentimentIntensityAnalyzer
+from pandas import DataFrame
+
+from aws_sale.cleaning.clean_data import clean_sentence_to_tokens, clean_tokens
+
+
+def _flatten_concatenation(matrix: List[List[str]]) -> List[str]:
+    flat_list = []
+    for row in matrix:
+        flat_list += row
+    return flat_list
+
+
+def frequencies(df: DataFrame, cleaner: Callable, callable_name: str, remove_stop_words: bool = True,
+                word_min_len: int = 3) -> FreqDist:
+    words = df['review_content'].apply(lambda s: clean_sentence_to_tokens(s, cleaner, callable_name)).tolist()
+    words = _flatten_concatenation(words)
+    if remove_stop_words:
+        words = clean_tokens(words, word_min_len)
+    return FreqDist(words)
+
+
+def sentiment_intensity_analysis(sentences: List[str]) -> List[dict]:
+    sia = SentimentIntensityAnalyzer()
+    results = []
+    for sentence in sentences:
+        results.append(sia.polarity_scores(sentence))
+    return results
+
+
+def _list_of_sent_to_df(list_of_sentiments: List[dict]) -> DataFrame:
+    sentiments = []
+    for sentiment in list_of_sentiments:
+        compound = sentiment["compound"]
+        if compound > 0.1:
+            sentiments.append('Positive')
+        elif compound < -0.1:
+            sentiments.append('Negative')
+        else:
+            sentiments.append('Neutral')
+    return DataFrame(sentiments, columns=['review_sentiment'])
+
+
+def plot_sentiments(list_of_sentiments: List[dict]):
+    df = _list_of_sent_to_df(list_of_sentiments)
+
+    sentiment_counts = df.value_counts()
+
+    sentiment_counts.plot(kind='bar', color=['green', 'blue', 'red'], title='Sentiment Analysis of Review Content')
+    plt.xlabel('Sentiment')
+    plt.ylabel('Count')
+    plt.savefig("j.png", format='png', dpi=150, bbox_inches='tight')
+    plt.show()
--- main.py
+++ main.py
@@ -4,9 +4,11 @@ from pandas import read_csv, DataFrame
 
 from aws_sale.cleaning import remove_na_and_duplicate, reformat_df, sentence_to_stemmed_tokens, \
     sentence_to_lemmatized_tokens
-from aws_sale.cleaning.clean_data import df_apply_cleaner_on_columns, clean_sentence_to_tokens
+from aws_sale.cleaning.clean_data import df_apply_cleaner_on_columns, clean_sentence_to_tokens, \
+    df_apply_cleaner_on_column
 from aws_sale.semantic import word2vec
 from aws_sale.semantic.word2vec import make_world_clusters, tsne_plot_similar_words, get_models_similarity
+from aws_sale.sentiment import frequencies, sentiment_intensity_analysis, plot_sentiments
 
 
 def lem_stem(df: DataFrame):
@@ -48,17 +50,29 @@ def prepare(df: DataFrame, column: str, cleaner: Callable, callable_name: str, w
     return word2vec(sentences, w2v_name)
 
 
+def w2v(df: DataFrame):
+    words = ['phone', 'good', 'photo']
+    w2v_skip = prepare(df, 'review_content', sentence_to_lemmatized_tokens, 'nltk', 'skipgramm')
+    w2v_cbow = prepare(df, 'review_content', sentence_to_lemmatized_tokens, 'nltk', 'cbow')
+    w2v_fasttext = prepare(df, 'review_content', sentence_to_lemmatized_tokens, 'nltk', 'fasttext')
+    df = get_models_similarity(words, [w2v_skip, w2v_cbow, w2v_fasttext], 10)
+    print(df)
+    embedding_clusters, word_clusters = make_world_clusters(words, w2v_cbow)
+    tsne_plot_similar_words(words, embedding_clusters, word_clusters)
+
+
+def sentiment(df: DataFrame):
+    fd = frequencies(df, sentence_to_lemmatized_tokens, 'nltk')
+    print(fd.most_common(3))
+    df = df_apply_cleaner_on_column(df, 'review_content', sentence_to_lemmatized_tokens, 'nltk')
+    results = sentiment_intensity_analysis(df['review_content_clean'].tolist())
+    plot_sentiments(results)
+
+
 df = read_csv("data/input/amazon.csv")
 df = remove_na_and_duplicate(df)
 df = reformat_df(df)
 print(df.head())
 # lem_stem(df)
-words = ['phone', 'good', 'photo']
-w2v_skip = prepare(df, 'review_content', sentence_to_lemmatized_tokens, 'nltk', 'skipgramm')
-w2v_cbow = prepare(df, 'review_content', sentence_to_lemmatized_tokens, 'nltk', 'cbow')
-w2v_fasttext = prepare(df, 'review_content', sentence_to_lemmatized_tokens, 'nltk', 'fasttext')
-df = get_models_similarity(words, [w2v_skip, w2v_cbow, w2v_fasttext], 10)
-print(df)
-embedding_clusters, word_clusters = make_world_clusters(words, w2v_cbow)
-
-tsne_plot_similar_words(words, embedding_clusters, word_clusters)
+# w2v(df)
+sentiment(df)
