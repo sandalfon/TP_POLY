41cb4d0785fca03113ce04034dd5374cefd95959
Gilles Vieira <vieira.gille@gmail.com>
2024-03-31 16:54:37 +0200

    change main for recommendation
    Signed-off-by: Gilles Vieira <vieira.gille@gmail.com>

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
+from random import random
 from typing import Callable
 
-from pandas import read_csv, DataFrame
+from pandas import DataFrame, read_csv
 
-from aws_sale.cleaning import remove_na_and_duplicate, reformat_df, sentence_to_stemmed_tokens, \
+from aws_sale.cleaning import sentence_to_stemmed_tokens, \
     sentence_to_lemmatized_tokens
 from aws_sale.cleaning.clean_data import df_apply_cleaner_on_columns, clean_sentence_to_tokens, \
-    df_apply_cleaner_on_column
+    df_apply_cleaner_on_column, remove_na_and_duplicate, reformat_df
+from aws_sale.recommendation.recommendation import prepare_df, get_avg_rating, get_sim_from_tfidf, \
+    get_product_recommendation
 from aws_sale.semantic import word2vec
 from aws_sale.semantic.word2vec import make_world_clusters, tsne_plot_similar_words, get_models_similarity
 from aws_sale.sentiment import frequencies, sentiment_intensity_analysis, plot_sentiments
@@ -69,10 +72,25 @@ def sentiment(df: DataFrame):
     plot_sentiments(results)
 
 
+def get_recommendation(df: DataFrame, cleaner: Callable, name: str, max_result, product_index: int) -> DataFrame:
+    df = prepare_df(df, cleaner, name)
+
+    avg_rating_df = get_avg_rating(df)
+    content_sim = get_sim_from_tfidf(df)
+    product_id = df['product_id'][product_index]
+    product_name = df['product_name'][product_index]
+    product_rating = avg_rating_df[avg_rating_df.index == product_id]['rating'].values[0]
+    recommended_products = get_product_recommendation(df, product_id, content_sim, avg_rating_df, max_result)
+    print("Recommendation for user who purchased product \"" + product_name + "\" with rating: " + str(product_rating))
+    return recommended_products
+
+
 df = read_csv("data/input/amazon.csv")
 df = remove_na_and_duplicate(df)
 df = reformat_df(df)
 print(df.head())
 # lem_stem(df)
 # w2v(df)
-sentiment(df)
+# sentiment(df)
+df_reco = get_recommendation(df, sentence_to_lemmatized_tokens, 'porter', 10, int(df.shape[1] * random()))
+print(df_reco.head())
