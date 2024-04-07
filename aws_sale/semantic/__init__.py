990e7b426908aa19ac519cc36cfc8b132e6386f0
Gilles Vieira <vieira.gille@gmail.com>
2024-04-07 14:45:39 +0200

    helper version
    Signed-off-by: Gilles Vieira <vieira.gille@gmail.com>

--- aws_sale/semantic/__init__.py
+++
@@ -1,1 +0,0 @@
-from aws_sale.semantic.word2vec import word2vec_skipgram, word2vec_cbow, word2vec
--- aws_sale/sentiment/sentiment.py
+++ aws_sale/sentiment/sentiment.py
@@ -1,27 +1,8 @@
-from typing import Callable, List
+from typing import List
 
 from matplotlib import pyplot as plt
-from nltk import FreqDist
 from nltk.sentiment import SentimentIntensityAnalyzer
 from pandas import DataFrame
-
-from aws_sale.cleaning.clean_data import clean_sentence_to_tokens, clean_tokens
-
-
-def _flatten_concatenation(matrix: List[List[str]]) -> List[str]:
-    flat_list = []
-    for row in matrix:
-        flat_list += row
-    return flat_list
-
-
-def frequencies(df: DataFrame, cleaner: Callable, callable_name: str, remove_stop_words: bool = True,
-                word_min_len: int = 3) -> FreqDist:
-    words = df['review_content'].apply(lambda s: clean_sentence_to_tokens(s, cleaner, callable_name)).tolist()
-    words = _flatten_concatenation(words)
-    if remove_stop_words:
-        words = clean_tokens(words, word_min_len)
-    return FreqDist(words)
 
 
 def sentiment_intensity_analysis(sentences: List[str]) -> List[dict]:
@@ -49,9 +30,10 @@ def plot_sentiments(list_of_sentiments: List[dict]):
     df = get_sentiment(list_of_sentiments)
 
     sentiment_counts = df.value_counts()
+    print(sentiment_counts)
 
     sentiment_counts.plot(kind='bar', color=['green', 'blue', 'red'], title='Sentiment Analysis of Review Content')
     plt.xlabel('Sentiment')
     plt.ylabel('Count')
-    plt.savefig("j.png", format='png', dpi=150, bbox_inches='tight')
+    plt.savefig("data/output/sentiments.png", format='png', dpi=150, bbox_inches='tight')
     plt.show()
