d0481b157a2c69b358e3b3d2986b55c0765127be
Gilles Vieira <vieira.gille@gmail.com>
2025-03-23 22:25:50 +0100

    rebase

--- aws_sale/cleaning/__init__.py
+++ aws_sale/cleaning/__init__.py
@@ -1,2 +1,7 @@
-from aws_sale.cleaning.clean_data import remove_na_and_duplicate, reformat_df, sentence_to_stemmed_tokens, \
-    sentence_to_lemmatized_tokens, df_apply_cleaner_on_column
+from aws_sale.cleaning.clean_data import (
+    remove_na_and_duplicate,
+    reformat_df,
+    sentence_to_stemmed_tokens,
+    sentence_to_lemmatized_tokens,
+    df_apply_cleaner_on_column,
+)
--- aws_sale/cleaning/clean_data.py
+++ aws_sale/cleaning/clean_data.py
@@ -16,21 +16,21 @@ def remove_na_and_duplicate(df: DataFrame) -> DataFrame:
 
 
 def reformat_df(df: DataFrame) -> DataFrame:
-    df['discounted_price'] = df['discounted_price'].astype(str).str.replace('₹', '').str.replace(',', '').astype(float)
-    df['actual_price'] = df['actual_price'].astype(str).str.replace('₹', '').str.replace(',', '').astype(float)
+    df["discounted_price"] = df["discounted_price"].astype(str).str.replace("₹", "").str.replace(",", "").astype(float)
+    df["actual_price"] = df["actual_price"].astype(str).str.replace("₹", "").str.replace(",", "").astype(float)
 
-    df['discount_percentage'] = df['discount_percentage'].astype(str).str.replace('%', '').astype(float)
+    df["discount_percentage"] = df["discount_percentage"].astype(str).str.replace("%", "").astype(float)
 
-    df['rating'] = to_numeric(df['rating'].astype(str).str.replace('|', ''), errors='coerce')
+    df["rating"] = to_numeric(df["rating"].astype(str).str.replace("|", ""), errors="coerce")
 
-    df['rating_count'] = df['rating_count'].astype(str).str.replace(',', '').astype(int)
+    df["rating_count"] = df["rating_count"].astype(str).str.replace(",", "").astype(int)
 
     return df
 
 
 def sentence_to_stemmed_tokens(sentence: str, stem_name: str = "porter") -> List[str]:
     if stem_name == "snowball":
-        sb = SnowballStemmer(language='english')
+        sb = SnowballStemmer(language="english")
         stemmer = sb.stem
     else:
         ps = PorterStemmer()
@@ -41,7 +41,7 @@ def sentence_to_stemmed_tokens(sentence: str, stem_name: str = "porter") -> List
 
 def sentence_to_lemmatized_tokens(sentence: str, lem_name: str = "nltk") -> List[str]:
     if lem_name == "spacy":
-        nlp = spacy.load('en_core_web_sm', disable=['parser', 'ner'])
+        nlp = spacy.load("en_core_web_sm", disable=["parser", "ner"])
         doc = nlp(sentence)
         return [token.lemma_ for token in doc]
     else:
@@ -52,13 +52,13 @@ def sentence_to_lemmatized_tokens(sentence: str, lem_name: str = "nltk") -> List
 
 def clean_sentence_to_tokens(sentence: str, cleaner: Callable, name: str):
     sentence = sentence.lower()
-    sentence = re.sub(r'[^a-zA-Z0-9\s]', '', sentence)
+    sentence = re.sub(r"[^a-zA-Z0-9\s]", "", sentence)
     tokens = cleaner(sentence, name)
     return tokens
 
 
 def clean_tokens(tokens: List[str], min_word_len: int = 3) -> List[str]:
-    stop_words = set(stopwords.words('english'))
+    stop_words = set(stopwords.words("english"))
     words = [word.lower() for word in tokens if word not in stop_words]
     words = [word for word in words if len(word) >= min_word_len]
     return words
@@ -66,16 +66,16 @@ def clean_tokens(tokens: List[str], min_word_len: int = 3) -> List[str]:
 
 def clean_sentence_to_str(sentence: str, cleaner: Callable, name: str, min_word_len: int = 3):
     tokens = clean_tokens(clean_sentence_to_tokens(sentence, cleaner, name), min_word_len)
-    text = ' '.join(tokens)
+    text = " ".join(tokens)
     return text
 
 
 def df_apply_cleaner_on_column(df: DataFrame, column: str, cleaner: Callable, name: str) -> DataFrame:
-    df[column + '_clean'] = df[column].apply(lambda s: clean_sentence_to_str(s, cleaner, name))
+    df[column + "_clean"] = df[column].apply(lambda s: clean_sentence_to_str(s, cleaner, name))
     return df
 
 
 def df_apply_cleaner_on_columns(df: DataFrame, columns: str, cleaner: Callable, name: str) -> DataFrame:
     for column in columns:
-        df[column + '_clean'] = df[column].apply(lambda s: clean_sentence_to_str(s, cleaner, name))
+        df[column + "_clean"] = df[column].apply(lambda s: clean_sentence_to_str(s, cleaner, name))
     return df
